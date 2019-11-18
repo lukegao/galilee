b3a1662ab2ff6475289ecfe14269cd6a4c09d7cc
lukegao <lukegao@gmail.com>
2019-11-18 15:49:26 +0800

    Get ready for postgres?

--- galilee/settings/production.py
+++ galilee/settings/production.py
@@ -24,6 +24,18 @@ AWS_DEFAULT_ACL = None
 # not sure about the effect of host headers
 ALLOWED_HOSTS = ['*']
 
+# postgres db settings
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
 
 try:
     from .local import *
